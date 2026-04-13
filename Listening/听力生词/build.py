451014bda2d67ad3d0d2ff9cfdbca09f7ade6805
superlls <1656839861@qq.com>
2026-04-13 15:22:18 +0800

    update: 生词本更新 2026-04-13 15:22

--- Listening/听力生词/build.py
+++ Listening/听力生词/build.py
@@ -1757,8 +1757,10 @@ document.addEventListener('keydown', e => {
   }
   else if (e.key === 'ArrowRight') go(1);
   else if (e.key === 'ArrowLeft') go(-1);
+  else if (e.key === 'l' || e.key === 'L') { e.preventDefault(); toggleStar(); }
 });
 
+applyMode();
 update();
 </script>
 </body>
